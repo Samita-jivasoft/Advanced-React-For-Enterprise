b4e93ed5e34cd3ef3dedcdd1a1d53e0232fba2ce
ShresthaSamita <samita.shrestha@jivasoft.com>
2024-08-29 14:06:39 -0500

    15.1: MONOREPOSITORY: Compiling and Running React component
    - Compiles and runs the simple button component
    
    - To run:
    
    `yarn dev`
    
    Runs in localhost:3000

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "@advanced-react-for-enterprise/react/lib";
+
+
+ReactDOM.render(
+  <Button label="Example Button" />,
+  document.getElementById('root') 
+);
