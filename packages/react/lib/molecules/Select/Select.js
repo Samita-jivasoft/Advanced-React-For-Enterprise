836683471c7c1ea24de6644341c129ed6d73d286
ShresthaSamita <samita.shrestha@jivasoft.com>
2024-09-03 09:36:22 -0500

    24: MONOREPOSITORY: Calculate overlay position

--- packages/react/lib/molecules/Select/Select.js
+++ packages/react/lib/molecules/Select/Select.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const Select = ({ options = [], label = 'Please select an option ...', onOptionSelected: handler }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const labelRef = useRef(null);
+    const [overlayTop, setOverlayTop] = useState(0);
     const onOptionSelected = (option, optionIndex) => {
         if (handler) {
             handler(option, optionIndex);
@@ -10,12 +12,15 @@ const Select = ({ options = [], label = 'Please select an option ...', onOptionS
     const onLabelClick = () => {
         setIsOpen(!isOpen);
     };
+    useEffect(() => {
+        setOverlayTop((labelRef.current?.offsetHeight || 0) + 10);
+    }, [labelRef.current?.offsetHeight]); // Calculating the position of the overlay based on button.
     return React.createElement("div", { className: 'dse-select' },
-        React.createElement("button", { className: 'dse-select__label', onClick: () => onLabelClick() },
+        React.createElement("button", { ref: labelRef, className: 'dse-select__label', onClick: () => onLabelClick() },
             React.createElement("span", null, label),
             React.createElement("svg", { width: '1rem', height: '1rem', fill: "none", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: 2, viewBox: "0 0 24 24", stroke: "currentColor" },
                 React.createElement("path", { d: "M19 9l-7 7-7-7" }))),
-        isOpen ? (React.createElement("ul", { className: 'dse-select__overlay' }, options.map((option, optionIndex) => {
+        isOpen ? (React.createElement("ul", { style: { top: overlayTop }, className: 'dse-select__overlay' }, options.map((option, optionIndex) => {
             return React.createElement("li", { onClick: () => onOptionSelected(option, optionIndex), key: option.value }, option.label);
         }))) : null);
 };
