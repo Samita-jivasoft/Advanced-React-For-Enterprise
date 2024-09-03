bb678bb4fc23b9e76a40be14ae7ab768c5a29afa
ShresthaSamita <samita.shrestha@jivasoft.com>
2024-09-03 11:09:24 -0500

    FIX *: UPDATED .gitignore file to ignore lib files
    *
    
    `git rm -r --cached .`
    `git add .`

--- packages/react/lib/atoms/Text/Text.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FontSizes from '../../foundation/FontSize';
-interface TextProps {
-    size?: keyof typeof FontSizes;
-    children?: React.ReactNode;
-}
-declare const Text: React.FC<TextProps>;
-export default Text;
