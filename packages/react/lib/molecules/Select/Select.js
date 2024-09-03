715c760ca8782eb7d4cd84ed0ac30d8706061ed3
ShresthaSamita <samita.shrestha@jivasoft.com>
2024-09-03 10:22:12 -0500

    27: MONOREPOSITORY: Animate `select` caret toggle

--- packages/react/lib/molecules/Select/Select.js
+++ packages/react/lib/molecules/Select/Select.js
@@ -26,7 +26,7 @@ const Select = ({ options = [], label = 'Please select an option ...', onOptionS
     return React.createElement("div", { className: 'dse-select' },
         React.createElement("button", { ref: labelRef, className: 'dse-select__label', onClick: () => onLabelClick() },
             React.createElement(Text, null, selectedOption === null ? label : selectedOption.label),
-            React.createElement("svg", { width: '1rem', height: '1rem', fill: "none", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: 2, viewBox: "0 0 24 24", stroke: "currentColor" },
+            React.createElement("svg", { className: `dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`, width: '1rem', height: '1rem', fill: "none", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: 2, viewBox: "0 0 24 24", stroke: "currentColor" },
                 React.createElement("path", { d: "M19 9l-7 7-7-7" }))),
         isOpen ? (React.createElement("ul", { style: { top: overlayTop }, className: 'dse-select__overlay' }, options.map((option, optionIndex) => {
             const isSelected = selectedIndex === optionIndex;
