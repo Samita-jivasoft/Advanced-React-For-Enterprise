5e44a128918a2b5bc161e4ede4d2768daf41f37f
ShresthaSamita <samita.shrestha@jivasoft.com>
2024-09-06 10:44:16 -0500

    monore XD2: ADDED: `tsconfig.json`  and render simple `Button`
    - Installed babel preset
    - The sample Button is in tsx
    - Without styled component

---
+++ packages/jivasoft-library/lib/components/atoms/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const Button = () => {
+   return (
+      <div>
+        I'll be a Button
+      </div>
+   )
+}
+export default Button;
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Button } from "@advanced-react-for-enterprise/react/lib"; 
+import { Button } from "@jivasoft/jivasoft-lib/lib/components/atoms/Button"; 
 
-import '@advanced-react-for-enterprise/scss/lib/Button.css'
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
     
     const root = createRoot(rootElement);
-    root.render(<Button label="Example Button" />);
+    root.render(<Button/>);
 } else {
     console.error("Root element not found.");
 }
